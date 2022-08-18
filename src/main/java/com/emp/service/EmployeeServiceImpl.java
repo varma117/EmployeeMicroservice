cd317d0f61d3ca94952d1d021cf7c973975d4217
unknown <cogjava3103@iiht.tech>
2022-08-18 10:45:46 +0530

    saving

---
+++ src/main/java/com/emp/service/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.emp.service;
+
+import java.util.List;
+
+import com.emp.entity.Employee;
+
+public class EmployeeServiceImpl implements IEmployeeService{
+	
+	List<Employee> list = List.of(
+		new Employee(123L,"Surya","998977889"),	
+		new Employee(124L,"Siva","998977887"),
+		new Employee(125L,"Varma","998977883"),
+		new Employee(126L,"Satya","998977882"),
+		new Employee(127L,"Vikram","998977880"));
+
+	@Override
+	public Employee getEmployee(Long id) {
+		
+		return list.stream().filter(employee -> employee.geteId().equals(id)).findAny().orElse(null);
+	}
+
+}
---
+++ src/main/java/com/emp/service/IEmployeeService.java
@@ -0,0 +1,9 @@
+package com.emp.service;
+
+import com.emp.entity.Employee;
+
+public interface IEmployeeService {
+	
+	public Employee getEmployee(Long id);
+
+}
